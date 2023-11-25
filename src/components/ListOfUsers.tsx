291677fcae0879ce582ff3f76d33006a6b043777
Emanuel Peire <emapeire@outlook.com>
2023-11-25 19:56:21 -0300

    Refactor CSS and import users data

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -7,64 +7,7 @@ import {
   TableHeaderCell,
   TableRow
 } from '@tremor/react'
-
-const users: {
-  id: string
-  name: string
-  email: string
-  github: string
-  actions: string
-}[] = [
-  {
-    id: '1',
-    name: 'Peter Doe',
-    email: 'peter@doe.com',
-    github: 'peterdoe',
-    actions: 'edit'
-  },
-  {
-    id: '2',
-    name: 'Lena Whitehouse',
-    email: 'lena@whitehouse.com',
-    github: 'lenawhitehouse',
-    actions: 'delete'
-  },
-  {
-    id: '3',
-    name: 'Phil Less',
-    email: 'phil@less.com',
-    github: 'philless',
-    actions: 'edit'
-  },
-  {
-    id: '4',
-    name: 'John Camper',
-    email: 'john@camper.com',
-    github: 'johncamper',
-    actions: 'edit'
-  },
-  {
-    id: '5',
-    name: 'Max Balmoore',
-    email: 'max@balmoore.com',
-    github: 'maxbalmoore',
-    actions: 'edit'
-  },
-  {
-    id: '6',
-    name: 'Peter Moore',
-    email: 'peter@moore.com',
-    github: 'petermoore',
-    actions: 'edit'
-  },
-  {
-    id: '7',
-    name: 'Joe Sachs',
-    email: 'joe@sachs.com',
-    github: 'joesachs',
-    actions: 'delete'
-  }
-]
+import { users } from '../data/users'
 
 export default function ListOfUsers() {
   return (
