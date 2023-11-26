fbc8da6ce00c31a751f60875a4e094874263180b
Emanuel Peire <emapeire@outlook.com>
2023-11-26 19:53:23 -0300

    Refactor form layout in CreateNewUser component

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -4,12 +4,12 @@ export default function CreateNewUser() {
   return (
     <Card className='mt-4'>
       <Title className='mb-4 ml-2'>Create New User</Title>
-      <form className=''>
+      <form className='flex flex-col gap-2'>
         <TextInput placeholder='Name' />
         <TextInput placeholder='E-mail' />
         <TextInput placeholder='Github username' />
         <div>
-          <Button type='submit' className='mt-4'>
+          <Button type='submit' className='mt-2'>
             Create
           </Button>
         </div>
