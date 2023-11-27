de57d69e60b76186d267bfc91a9a035807408c99
Emanuel Peire <emapeire@outlook.com>
2023-11-26 21:03:31 -0300

    Add Toaster component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import CreateNewUser from './components/CreateNewUser'
 import ListOfUsers from './components/ListOfUsers'
+import { Toaster } from 'sonner'
 
 export default function App() {
   return (
     <main>
       <ListOfUsers />
       <CreateNewUser />
+      <Toaster richColors />
     </main>
   )
 }
